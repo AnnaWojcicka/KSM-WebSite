d0b4a5625cc9bac2003e24814e791f41721c1430
Rafał Wójcicki <anna.spodymek1@gmail.com>
2020-08-18 19:39:56 +0200

    Second step of my work

---
+++ js/mobile__button.js
@@ -0,0 +1,11 @@
+(() => {
+    const button = document.querySelector(".js-main-nav__mobile-button");
+    const menu = document.querySelector(".js-main-nav__menu")
+
+    const toggleClass = () => {
+        menu.classList.toggle("main-nav__menu--open");
+    };
+
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+})(); 
